1c00983a0a0b51551b4032b573852d2566dba93d
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-04-20 15:32:26 +0600

    fix port

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ function saveLocationForTests(lat, lon) {
 	});
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
