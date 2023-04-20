d75be4fa364345ceabd5e7be51e2ca30b1c52e15
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-04-20 16:19:19 +0600

    major restructure

--- server.js
+++ server.js
@@ -21,20 +21,34 @@ app.get("/", (req, res) => {
 	});
 });
 
-app.get("/weather", (req, res) => {
+app.get("/weather", async (req, res) => {
 	const lat = req.query.lat;
 	const lon = req.query.lon;
 	logData(lat, lon);
 
-	fs.readFile("weather.html", function (err, data) {
-		if (err) {
-			res.writeHead(404, { "Content-Type": "text/html" });
-			return res.end("404 Not Found");
-		}
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write(data);
-		return res.end();
-	});
+	try {
+		const fetch = await import("node-fetch");
+		const apiKey = "1c3296cf2d396c47f7abfd8a324b66cb";
+		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+		const response = await fetch.default(url);
+		const data = await response.json();
+
+		const temperature = data.main.temp;
+		const description = data.weather[0].description;
+		const city = data.name;
+
+		const weatherData = {
+			city: city,
+			temp: temperature,
+			desc: description,
+		};
+
+		res.status(200).json(weatherData);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Something went wrong" });
+	}
 });
 
 // For DEBUG PURPOSES
--- static/script.js
+++ static/script.js
@@ -1,33 +1,14 @@
-// URL Parse /////////////////////////////////
-const currentUrl = window.location.href;
-const url = new URL(currentUrl);
-
 function success(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
-
-	const link = document.createElement("a");
-	link.href = `./weather?lat=${lat}&lon=${lon}`;
-	link.id = "check";
-	document.getElementById('linker').appendChild(link);
-	document.getElementById("check").click();
-}
-
-function error() {
-	console.error("Unable to retrieve location.");
-}
-
-function getWeatherData(lat, lon) {
-	const apiKey = "1c3296cf2d396c47f7abfd8a324b66cb";
-	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-	console.log(url);
+	const url = `/weather?lat=${lat}&lon=${lon}`;
 
 	fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
-			const temperature = data.main.temp;
-			const description = data.weather[0].description;
-			const city = data.name;
+			const temperature = data.temp;
+			const description = data.desc;
+			const city = data.city;
 
 			const weatherData = `
       <h2>${city}</h2>
@@ -35,9 +16,21 @@ function getWeatherData(lat, lon) {
       <p>Description: ${description}</p>
     `;
 
-			document.getElementById("weather-data").innerHTML = weatherData;
+			const button = document.getElementById("check-btn");
+			const cont = button.parentElement;
+			const output = document.createElement('div');
+			output.id = "weather-data";
+			output.innerHTML = weatherData;
+
+			cont.removeChild(button);
+			cont.appendChild(output);
+
 		})
 		.catch((error) => console.error(error));
+}
+
+function error() {
+	console.error("Unable to retrieve location.");
 }
 
 function checkWeather() {
